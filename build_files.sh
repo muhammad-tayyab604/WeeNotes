e0477f2ffb5767639f361185041e563fda0831ff
Muhammad Tayyab <hackert960@@gmail.com>
2023-04-08 16:01:22 +0500

    Update

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,4 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
